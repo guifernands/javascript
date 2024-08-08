eda75ba8e29c754bb7489789b5e96a2d2bef76d4
guifernands <guiiilherme.fernandes@gmail.com>
2024-08-08 02:31:07 -0300

    aula 11 completa

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+vel = 40.2
+
+console.log(`A velocidade do seu carro é de ${vel} Km/h.`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. Você tem uma multa de R$500.00 pendente. Consulte o Detran para o pagamento da multa.`)
+}   else {
+    console.log(`Continue dirigindo com cautela!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
