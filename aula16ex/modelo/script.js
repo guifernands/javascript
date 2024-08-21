567826703f009f4eff2aebcaa2ddfba2b8e791a0
guifernands <guiiilherme.fernandes@gmail.com>
2024-08-21 20:55:13 -0300

    a

---
+++ aula16ex/modelo/script.js
@@ -0,0 +1,12 @@
+let num = document.querySelector('input#numero')
+let lista = document.querySelector('select#sel')
+let res = document.querySelector('div#res')
+let valores = []
+
+function adicionar() {
+    if(isNumero(num.value) && !lista(num.value)) {
+
+    } else {
+        window.alert
+    }
+}
