c081f37b606c599cfcd7fa75005074c7507d44bc
guifernands <guiiilherme.fernandes@gmail.com>
2025-10-13 20:26:55 -0300

    aula 41 arrow function e op ternaria

---
+++ js-udemy/aula41/index.js
@@ -0,0 +1,11 @@
+/* 
+function qualOMaior(x, y) {
+    if (x > y) return x;
+    return y;
+
+    return x > y ? x : y;
+} 
+*/
+
+const qualOMaior2 = (x, y) => x > y ? x : y;
+console.log(qualOMaior2(22, 11));
