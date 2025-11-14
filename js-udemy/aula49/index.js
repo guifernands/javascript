463785939beab4fcc64f0746c646dd5b39acd502
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-11-13 21:11:20 -0300

    varios tipos de functions

---
+++ js-udemy/aula49/index.js
@@ -0,0 +1,32 @@
+// declaração de função (function hoisting)
+falaOi();
+function falaOi() {
+    console.log('oie');
+}
+
+// first-class objects (objetos de primeira classe)
+// function expression
+const souUmDado = function() {
+    console.log('sou um dado')
+};
+souUmDado();
+
+function executaFuncao(funcao) {
+    console.log('executando sua funcão abaixo:');
+    funcao();
+}
+executaFuncao(souUmDado)
+
+// arrow function
+const funcaoArrow = () => {
+    console.log('sou uma arrow function')
+}
+funcaoArrow();
+
+// dentro de um objeto
+const obj = {
+    falar: function() {
+        console.log('estou falando...(de dreto do objeto)')
+    }
+};
+obj.falar();
