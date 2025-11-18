52d3368d498b2f26e5c0c661bb07dbc576eb89a4
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-11-18 20:59:26 -0300

    Update index.js

--- js-udemy/aula56/index.js
+++ js-udemy/aula56/index.js
@@ -5,20 +5,33 @@ function criaPessoa(nome, sobrenome, a, p) {
     return {
         nome,
         sobrenome,
-        fala: function(assunto) {
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+        },
+        fala(assunto = 'falando sobre gatos') {
             return `${this.nome} está ${assunto}.`;
         },
         altura: a,
         peso: p,
-        imc() {
+        // Getter
+        get imc() {
             const indice = this.peso / (this.altura ** 2); 
             return indice.toFixed(2);
         }
     };
 }
 
-const p1 = criaPessoa('Gui', 'Fernandes', 1.78, 65);
-console.log(p1.imc());
-const p2 = criaPessoa('Cláudia', 'Fernandes', 1,68, 70);
-console.log(p2.imc());
-console.log(p1.fala('falando sobre JS'));
+const p1 = criaPessoa('Guilherme', 'Fernandes', 1.78, 65);
+const p2 = criaPessoa('Ana Luiza', 'Rodrigues', 1.40, 25);
+const p3 = criaPessoa('Cláudia', 'Rejane Ferreira', 1.68, 90);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
