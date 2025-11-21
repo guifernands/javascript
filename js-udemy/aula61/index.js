42682f49d400135888a890d5256ab79c5823d1cf
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-11-21 15:02:24 -0300

    aula61 incompleta

---
+++ js-udemy/aula61/index.js
@@ -0,0 +1,13 @@
+// função geradora
+function* geradora1() {
+    // codigo...
+    yield 'valor 1'
+    // codigo...
+    yield 'valor 2'
+    // codigo...
+    yield 'valor 3'
+}
+
+function* geradora2() {
+    
+}
