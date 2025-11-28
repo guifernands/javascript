4a37c9c2024020552d319698b8772269fb2efc7e
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-11-27 22:27:41 -0300

    array map

---
+++ js-udemy/aula66/index.js
@@ -0,0 +1,28 @@
+// dobre os números
+//
+/* const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11];
+const numerosEmDobro = numeros.map(valor => valor * 2);
+console.log(numerosEmDobro); */
+
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+];
+
+const nomes = pessoas.map(obj => obj.nome);
+console.log(nomes);
+
+const idades = pessoas.map(obj => ({ idade: obj.idade }));
+console.log(idades);
+
+const comIds = pessoas.map(function(obj, indice) {
+    const newObj = {...obj};
+    newObj.id = indice;
+    return newObj;
+});
+console.log(comIds);
