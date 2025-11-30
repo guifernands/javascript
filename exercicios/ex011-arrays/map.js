b73c076212c47680b892a2e89e0c5af22ee0db87
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-11-30 00:16:04 -0300

    training array map, reduce and filter

--- exercicios/ex011-arrays/map.js
+++ exercicios/ex011-arrays/map.js
@@ -1,15 +1,21 @@
 const products = [
-    { id: 1, nome: 'baixo', categoria: 'instrument', preco: 500},
-    { id: 2, nome: 'saxofone', category: 'instrument', preco: 900},
-    { id: 3, nome: 'flauta', category: 'instrument', preco: 130},
-    { id: 4, nome: 'amplificador', category: 'eletronics', preco: 500},
-    { id: 5, nome: 'cabo de audio', category: 'eletronics', preco: 40},
-    { id: 6, nome: 'camiseta metalica', category: 'roupas', preco: 100},
-    { id: 7, nome: 'colete guns & roses', category: 'roupas', preco: 250}
+    { id: 1, name: 'bass', categoria: 'instrument', price: 500},
+    { id: 2, name: 'saxofone', category: 'instrument', price: 900},
+    { id: 3, name: 'flute', category: 'instrument', price: 130},
+    { id: 4, name: 'amplifier', category: 'eletronics', price: 400},
+    { id: 5, name: 'audio cables', category: 'eletronics', price: 40},
+    { id: 6, name: 'shirt metalica', category: 'clothes', price: 100},
+    { id: 7, name: 'vest guns & roses', category: 'clothes', price: 250}
 ];
 
-
+// return only eletronics
 const eletronics = products.filter(obj => obj.category === 'eletronics');
 
-const discount10 = eletronics.map(obj => obj.price - (obj.price * 0.1));
-console.log(discount10);
+// make the discount in the eletronics
+const discount10 = eletronics.map(obj => obj.price * 0.9);
+
+// return the total with the discount
+const total = discount10.reduce(function(acc, obj) {
+    return acc + obj; 
+}, 0); // 0 to define the acumulator
+console.log(total);
