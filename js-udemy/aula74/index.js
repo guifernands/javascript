3b6b5d7226e01bcb51af5f3e1ac2ecc6111c1b4c
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-12-04 09:59:32 -0300

    prototypes

--- js-udemy/aula74/index.js
+++ js-udemy/aula74/index.js
@@ -2,8 +2,14 @@
 function Person(name, surname) {
     this.name = name;
     this.surname = surname;
-    this.fullName = () => this.name + ' ' + this.surname;
+    // default but descrease performance because all of the persons ll have the function
+    // this.fullName = () => 'ORIGINAL ',this.name + ' ' + this.surname;
 }
+
+// Person.prototype === person1.__proto__; -> true
+
+// improves performance by adding the function to the prototype
+Person.prototype.fullName = () => this.nome + ' ' + this.sobrenome;
 
 // instace
 const person1 = new Person('Gui', 'O.'); // <- Person = Constructor Function
@@ -11,3 +17,5 @@ const person2 = new Person('Fernanda', 'A.'); // <- Person = Constructor Functio
 
 console.dir(person1);
 console.dir(person2);
+
+// person1 -> Person.prototype -> Object.prototype
