c5d16cbe4ac49b8bc4dd029ca128a7b48f40597b
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-12-06 13:18:24 -0300

    cpf validator part2

--- exercicios/ex017-cpf-validator/index.js
+++ exercicios/ex017-cpf-validator/index.js
@@ -1,7 +1,9 @@
 function cpfCalc() {
-    const cpf = '60161764002';
+    const cpf = '70548445052';
     let calculated = [];
-    let total = 0;
+    let calculated2 = [];
+    let total1 = 0;
+    let total2 = 0;
 
     for(let i = 0; i <= 9; i++) {
         let temp = parseInt(cpf[i]);
@@ -10,9 +12,25 @@ function cpfCalc() {
     }
     console.log(calculated);
 
-    for (let i = 0; i <= 9; i++) {
-        total += calculated[i];
+    for (let i = 0; i <= 8; i++) {
+        total1 += calculated[i];
     }
+
+    let firstDigit = 11 - (total1 % 11);
+    if (firstDigit > 9) {
+        firstDigit = 0;
+    }    
+    for(let i = 0; i <= 9; i++) {
+        let temp = parseInt(cpf[i]);
+        let multiplier = 11 - i;
+        calculated2.push(temp * multiplier);
+    }
+    for (let i = 0; i <= 9; i++) {
+        total2 += calculated2[i];
+    }
+    console.log(calculated2);
+
+
 }
 
 
