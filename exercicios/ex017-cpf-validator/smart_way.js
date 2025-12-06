b5a379c2a7dbea9f464a3181af545e2e979c85de
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-12-06 19:20:17 -0300

    cpf validator complete

--- exercicios/ex017-cpf-validator/smart_way.js
+++ exercicios/ex017-cpf-validator/smart_way.js
@@ -10,15 +10,19 @@ function CpfValidator(cpfSent) {
 CpfValidator.prototype.validator = function() {
     if(typeof this.cpfClean === 'undefined') return false;
     if(this.cpfClean.length !== 11) return false;
+    if(this.isSequence()) return false;
 
     const parcialCpf = this.cpfClean.slice(0, -2);
     const digit1 = this.digitCreator(parcialCpf);
-
-    return true; 
+    const digit2 = this.digitCreator(parcialCpf + digit1);
+    
+    const newCpf = parcialCpf + digit1 + digit2;
+    return newCpf === this.cpfClean;
 };
 
 CpfValidator.prototype.digitCreator = function(parcialCpf) {
     const cpfArray = Array.from(parcialCpf);
+
     let regressive = cpfArray.length + 1;
     let total = cpfArray.reduce((ac, val) => {
         console.log(regressive, val, regressive * val);
@@ -28,7 +32,19 @@ CpfValidator.prototype.digitCreator = function(parcialCpf) {
     }, 0);
 
     const digit = 11 - (total % 11);
+    return digit > 9 ? '0' : String(digit);
 };
 
-const cpf = new CpfValidator('705.484.450-52');
-console.log(cpf.validator());
+CpfValidator.prototype.isSequence = function() {
+    const sequence = this.cpfClean[0].repeat(this.cpfClean.length);
+    return sequence === this.cpfClean;
+};
+
+const cpf = new CpfValidator('070.987.720-03');
+
+if (cpf.validator()) {
+    console.log('Valid CPF.')
+}
+else {
+    console.log('Invalid CPF.')
+}
