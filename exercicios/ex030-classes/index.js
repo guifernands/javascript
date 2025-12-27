c8cada56a4b5c69256fbc6220f7bbc8ebf3ce916
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-12-27 01:38:25 -0300

    Update index.js

--- exercicios/ex030-classes/index.js
+++ exercicios/ex030-classes/index.js
@@ -66,14 +66,39 @@ class Jogador extends Usuario {
     }
 }
 
-const user1 = new Usuario('Guile', 'guifernandes@gmail.com');
-const user2 = new Usuario('Askelad', 'askelad321@hotmail.com');
-const user3 = new Usuario('Knight', 'knight000@outlook.com');
+class Admin extends Usuario {
+    #permissoes;
 
-const bancoDeDados = [user1, user2, user3];
+    constructor(nome, email, permissoes) {
+        super(nome, email);
+        this.#permissoes = [permissoes];
+    }
 
-// 3. Testando o método Estático
-const resultadoBusca = Usuario.buscarPorEmail(bancoDeDados, 'askelad321@hotmail.com');
+    banir(jogadorAlvo) {
+        jogadorAlvo.resetar();
+        console.log(`O jogador ${jogadorAlvo.nome} foi banido e teve seus status resetados.`);
+    }
 
-console.log(resultadoBusca); 
-// Deve imprimir o objeto Usuario do Askelad
+    exibirInfo() {
+        return `Admin ${this.nome} - Permissões: ${this.#permissoes.length}`;
+    }
+}
+
+// --- ZONA DE TESTES (A MÁGICA ACONTECE AQUI) ---
+
+// 1. Criando os personagens
+const player1 = new Jogador('Guilherme', 'gui@gmail.com', 'GuiSlayer99');
+const adm1 = new Admin('Roberto', 'roberto@ti.com', ['banir', 'editar', 'excluir']);
+
+player1.ganharPontos(1200); 
+console.log(player1.exibirInfo());
+
+console.log(adm1.exibirInfo());
+adm1.banir(player1);
+
+// reseta status
+console.log(player1.exibirInfo());
+
+// estático
+const lista = [player1, adm1];
+console.log(Usuario.buscarPorEmail(lista, 'roberto@ti.com'));
