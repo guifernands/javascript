143c3c422d4fba4f2091fec3b766029cafbd89ff
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-12-26 23:25:50 -0300

    class usuario

---
+++ exercicios/ex030-classes/index.js
@@ -0,0 +1,50 @@
+class Usuario {
+    // campos privados
+    #nome;
+    #email;
+
+    constructor(nome, email) {
+        this.#nome = nome;
+        this.#email = email;
+    }
+
+    get nome() {
+        return this.#nome; // retorna o valor privado
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    set nome(nome) {
+        if(nome.length < 4) {
+            throw new Error('Nome curto');
+        }
+        this.#nome = nome;
+    }
+
+    exibirInfo() {
+        return `Usuário: ${this.#nome} (${this.#email})`;
+    }
+
+    static buscarPorEmail(listaDeUsuarios, emailBuscado) {
+        for(const usuario of listaDeUsuarios) {
+            if(usuario.email === emailBuscado) {
+                return usuario;
+            }
+        }
+        return undefined;
+    }    
+}
+
+const user1 = new Usuario('Guile', 'guifernandes@gmail.com');
+const user2 = new Usuario('Askelad', 'askelad321@hotmail.com');
+const user3 = new Usuario('Knight', 'knight000@outlook.com');
+
+const bancoDeDados = [user1, user2, user3];
+
+// 3. Testando o método Estático
+const resultadoBusca = Usuario.buscarPorEmail(bancoDeDados, 'askelad321@hotmail.com');
+
+console.log(resultadoBusca); 
+// Deve imprimir o objeto Usuario do Askelad
