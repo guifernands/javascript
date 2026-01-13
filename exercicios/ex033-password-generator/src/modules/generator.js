b8525e26b2eb02269073756f04b01bdc614a5db4
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-01-12 22:07:56 -0300

    password generator

---
+++ exercicios/ex033-password-generator/src/modules/generator.js
@@ -0,0 +1,18 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const genUpper = () => String.fromCharCode(rand(65, 91));
+const genLower = () => String.fromCharCode(rand(97, 123));
+const genNumber = () => String.fromCharCode(rand(48, 58));
+const symbols = '!@#$%^&*(){}[.,];:/><_+=-~`"';
+const genSymbol = () => symbols[rand(0, symbols.length)];
+
+function genPassword(length, upper, lower, num, sym) {
+    const pwArray = [];
+
+    // if upper, lower, num, sym are true.. executes...
+    for(let i = 0; i <= length; i++) {
+        upper && pwArray.push(genUpper());
+        lower && pwArray.push(genLower());
+        num && pwArray.push(genNumber());
+        sym && pwArray.push(genSymbol());
+    }
+}
