ae2406ce9e2df03ae8ab2b91ca5a6019d42791ff
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-01-12 22:22:47 -0300

    Update generator.js

--- exercicios/ex033-password-generator/src/modules/generator.js
+++ exercicios/ex033-password-generator/src/modules/generator.js
@@ -1,3 +1,5 @@
+const { join } = require("core-js/core/array");
+
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const genUpper = () => String.fromCharCode(rand(65, 91));
 const genLower = () => String.fromCharCode(rand(97, 123));
@@ -9,10 +11,13 @@ function genPassword(length, upper, lower, num, sym) {
     const pwArray = [];
 
     // if upper, lower, num, sym are true.. executes...
-    for(let i = 0; i <= length; i++) {
+    for(let i = 0; i < length; i++) {
         upper && pwArray.push(genUpper());
         lower && pwArray.push(genLower());
         num && pwArray.push(genNumber());
         sym && pwArray.push(genSymbol());
     }
+    console.log(pwArray.join(''));
 }
+
+genPassword(10, true, true, true, true); 
