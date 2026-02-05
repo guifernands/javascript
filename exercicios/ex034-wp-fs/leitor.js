ec7e2e62a138e0b7c6e32f16f46d63d81c24727f
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-02-05 18:33:55 -0300

    fs exercise completed

--- exercicios/ex034-wp-fs/leitor.js
+++ exercicios/ex034-wp-fs/leitor.js
@@ -3,26 +3,18 @@ const fs = require('fs').promises; // versão moderna com promises
 
 async function main() {
   try {
-    let texto = await fs.readFile('mensagem.txt', 'utf8');
-    
-    const textoOriginal = texto;
+    let textoOriginal = await fs.readFile('mensagem.txt', 'utf8');
     console.log('Li isto:', textoOriginal);
 
-    texto = texto.toUpperCase();
+    textoOriginal = textoOriginal.toUpperCase(); // letras maiúsculas
     
-    const textoFinal = texto;
+    const textoFinal = textoOriginal;
+    await fs.writeFile('mensagem.txt', textoFinal); // substitui o arquivo por um com o novo texto
 
-    // PASSO 3: Guardar num novo ficheiro chamado 'mensagem_caps.txt'
-    // Dica: Usa o método await fs.writeFile(..., ...)
-    
-    /* O TEU CÓDIGO AQUI */
+    console.log('Ficheiro criado com sucesso, novo texto:', textoFinal);
 
-    fs.writeFile('mensagem.txt', textoFinal)
-
-    console.log('Ficheiro criado com sucesso!');
-
-  } catch (erro) {
-    console.error('Ops, algo correu mal:', erro);
+  } catch (e) {
+    console.error('Ops, algo correu mal:', e);
   }
 }
 
