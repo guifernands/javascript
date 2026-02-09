7203c250112700b583229695ef7980015dbcce60
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-02-09 17:40:00 -0300

    fs and wp exercises (explained)

---
+++ exercicios/ex034-fs-wp-exercises/ex03-wp-fs/gerar-versao.js
@@ -0,0 +1,8 @@
+// importa o módulo "File System" do node, o .promises serve para usarmos a verão mais moderna usando await ao envés da antiga usando callbacks
+const fs = require('fs').promises;
+
+async function criarArquivoDeVersao() {
+    const dados = await fs.readFile('./package.json', 'utf8'); // utf8 pra ler como letras (sem isso ele lê em binário)
+
+    const json = JSON.parse(dados);
+}
