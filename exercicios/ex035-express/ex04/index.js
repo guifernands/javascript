08e3af26f1e5a0ba3b63e44a0f76d07d78cbc3b8
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-03-05 19:45:06 -0300

    middleware

---
+++ exercicios/ex035-express/ex04/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+// Middleware nativo para ler JSON
+app.use(express.json());
+
+// "banco de dados"
+let jogos = [
+    { id: 1, titulo: "Dark Souls", genero: "Souls-like" },
+    { id: 2, titulo: "Don't Starve", genero: "Survival" },
+    { id: 3, titulo: "Counter-Strike", genero: "FPS" }
+];
+
+function validarJogo(req, res, next) {
+    if(req.body.titulo && req.body.genero) {
+        next();
+    }
+    else {
+        res.status(400).json({ erro: "[ERRO] Título e/ou gênero inválido(s)." });
+    }
+}
+
+app.post('/jogos', validarJogo, (req, res) => {
+    // o que foi enviado de fora
+    const dadosRecebidos = req.body;
+
+    const novoJogo = {
+        id: jogos.length + 1,
+        titulo: dadosRecebidos.titulo,
+        genero: dadosRecebidos.genero
+    }
+
+    //adicionado ao "banco de dados"
+    jogos.push(novoJogo);
+
+    // mostra o jogo novo que foi salvo
+    res.status(201).json(novoJogo);
+});
+
+// iniciando o servidor / "fica ouvindo" requisições na porta 3000 do computador
+app.listen(3000, () => {
+    console.log("Servidor rodando!");
+    console.log("Acessar http://localhost:3000")
+});
+
+// usar esse código no terminal para atualizar toda vez que o código muda!
+// node --watch index.js
