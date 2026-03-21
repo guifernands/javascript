20c3e0a933a2314da350f8936851f73afb4c8069
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-03-21 20:01:09 -0300

    Update server.js

--- js-udemy/node/aula17/server.js
+++ js-udemy/node/aula17/server.js
@@ -22,6 +22,7 @@ const { meuMiddleware, checkCsrfError, csrfMiddleware } = require('./src/middlew
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }) );
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
