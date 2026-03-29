4c03484a3cab0f1deb70797ce4644ac5004df5c1
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-03-29 12:53:10 -0300

    css

--- exercicios/ex036-agendaproject/frontend/main.js
+++ exercicios/ex036-agendaproject/frontend/main.js
@@ -6,11 +6,36 @@ import Login from './modules/Login'; // está vermelho mas está certo
 
 import Contato from './modules/Contato';
 
+import './assets/css/style.css';
+
+// trocar de tema
+const btnSwitch = document.querySelector('#theme-switch');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  btnSwitch.innerHTML = '☀️ Modo Claro';
+}
+
+btnSwitch.addEventListener('click', () => {
+  let theme = document.documentElement.getAttribute('data-theme');
+  
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'light');
+    btnSwitch.innerHTML = '🌙 Modo Escuro';
+    localStorage.setItem('theme', 'light');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    btnSwitch.innerHTML = '☀️ Modo Claro';
+    localStorage.setItem('theme', 'dark');
+  }
+});
+
+// forms
 const cadastroContato = new Contato('.form-contato');
 const login = new Login('.form-login');
 const cadastro = new Login('.form-cadastro');
+
 login.init();
 cadastro.init();
 cadastroContato.init();
-
-/* import './assets/css/style.css'; */
--- exercicios/ex036-agendaproject/webpack.config.js
+++ exercicios/ex036-agendaproject/webpack.config.js
@@ -8,16 +8,23 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            exclude: /node_modules/,
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/env'] 
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'] 
+                    },
                 }
+            },
+
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             }
-        }]
+        ]
     },
     devtool: 'source-map'
 };
