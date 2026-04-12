f768802202a22a4a484a822d92bf86f9994e01ec
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-04-12 12:15:57 -0300

    Default functions, props, {destructuring} & spread operator

--- js-alura/tecboard/src/App.jsx
+++ js-alura/tecboard/src/App.jsx
@@ -2,15 +2,74 @@ import './App.css'
 
 // no react, componentes são FUNÇÕES
 
-function FormularioDeEvento() {
+// props é um OBJETO
+// props.ALGUMACOISA
+function TituloFormulario(props) {
+  return (
+      <h2>
+        { props.children }
+      </h2>
+  )
+}
+
+// { desestruturação } (melhor método)
+function CampoDeFormulario({ children }) {
+  return (
+    <fieldset>
+      { children }
+    </fieldset>
+  )
+}
+
+function Label({ children, htmlFor }) {
+  return (
+    <label htmlFor={ htmlFor }>
+      { children }
+    </label>
+  )
+}
+
+// jeito burro
+/* function CampoDeEntrada({ type, name, id, placeholder}) {
 
   return (
+    <input
+      type= { type }
+      name= { name }
+      id= { id }
+      placeholder= { placeholder }
+    />
+  )
+} */
+
+// jeito inteligente
+function CampoDeEntrada(props) {
+
+  return (
+    <input { ...props }/>
+  )
+}
+
+function FormularioDeEvento() {
+  
+  return (
     <form className='form-evento'>
-      <h2>Preencha para criar um evento:</h2>
-      <fieldset>
-        <label htmlFor="nome">Qual o nome do evento?</label>
-        <input type="text" id='nome' />
-      </fieldset>
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
+
+      <CampoDeFormulario>
+        <Label>
+          Qual o nome do evento?
+        </Label>
+
+        <CampoDeEntrada
+          type="text"
+          name="nomeEvento"
+          id='nome'
+          placeholder='Summer dev hits'
+        />
+      </CampoDeFormulario>
     </form>
   )
 }
